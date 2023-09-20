0505e647ea1c35753f684a336c538e8040776e2a
Tomislav Andric <tomislavandric11@gmail.com>
2023-09-20 21:26:18 +0200

    initial commit

---
+++ src/main/java/com/leagueresults/LeagueResultsApplication.java
@@ -0,0 +1,13 @@
+package com.leagueresults;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueResultsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueResultsApplication.class, args);
+	}
+
+}
